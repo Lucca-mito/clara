6d7dfa0382435c38a9ff8b17bc58476672e50a69
Lucca-mito <luccasukman@gmail.com>
2018-05-28 12:47:51 -0300

    Refatorei bastante código

--- Unix/test_transpiler.py
+++ Unix/test_transpiler.py
@@ -5,65 +5,66 @@ from transpiler import transpile
 # Unit testing for the Clara transpiler.
 class TranspilerTDD(unittest.TestCase):
     def test_adds_encoding(self):
-        source = ''
-        result = transpile(source);
+        source   = ''
         expected = '#coding: utf-8\n'
-        self.assertEqual(result, expected)
+        self.assertEqual(expected, transpile(source))
 
     def test_period_to_semicolon(self):
-        source = 'something.'
-        expected = (
-            '#coding: utf-8\n'
-            'something;'
-        )
+        source   =  'something.'
+        expected = ('#coding: utf-8\n'
+                    'something;')
         self.assertEqual(expected, transpile(source))
 
     def test_assignment_without_accent(self):
-        source = 'minha_idade eh 17'
-        expected = (
-            '#coding: utf-8\n'
-            'minha_idade = 17'
-        )
+        source   =  'minha_idade eh 17'
+        expected = ('#coding: utf-8\n'
+                    'minha_idade = 17')
         self.assertEqual(expected, transpile(source))
 
     def test_assignment_with_accent(self):
-        source = 'minha_idade é 17'
-        expected = (
-            '#coding: utf-8\n'
-            'minha_idade = 17'
-        )
+        source   =  'minha_idade é 17'
+        expected = ('#coding: utf-8\n'
+                    'minha_idade = 17')
+        self.assertEqual(expected, transpile(source))
+
+    def test_array_assignment(self):
+        source   = ('numeros são 1, 2, 3\n'
+                    'um, dois, tres são numeros')
+        expected = ('#coding: utf-8\n'
+                    'numeros = 1, 2, 3\n'
+                    'um, dois, tres = numeros')
         self.assertEqual(expected, transpile(source))
 
     def test_excludes_strings(self):
-        source = '"mostra quem vc eh!"'
-        expected = (
-            '#coding: utf-8\n'
-            '"mostra quem vc eh!"'
-        )
+        source   =  '"mostra quem vc eh!"'
+        expected = ('#coding: utf-8\n'
+                    '"mostra quem vc eh!"')
         self.assertEqual(expected, transpile(source))
 
     def test_supports_escaped_quotes(self):
-        source = 'mostra "\\"Não perca a cabeça\\" - Robespierre"'
-        expected = (
-            u'#coding: utf-8\n'
-            u'print "\\"Não perca a cabeça\\" - Robespierre"'
-        )
+        source   =   'mostra "\\"Não perca a cabeça\\" - Robespierre"'
+        expected = (u'#coding: utf-8\n'
+                    u'print "\\"Não perca a cabeça\\" - Robespierre"')
         self.assertEqual(expected, transpile(source))
 
     def test_equality_operator(self):
-        source = 'a = b'
-        expected = (
-            '#coding: utf-8\n'
-            'a == b'
-        )
+        source   =  'a = b'
+        expected = ('#coding: utf-8\n'
+                    'a == b')
         self.assertEqual(expected, transpile(source))
 
     def test_inequality_operator(self):
-        source = 'a =/= b'
-        expected = (
-            '#coding: utf-8\n'
-            'a != b'
-        )
+        source   =  'a =/= b'
+        expected = ('#coding: utf-8\n'
+                    'a != b')
+        self.assertEqual(expected, transpile(source))
+
+    def test_function_definition(self):
+        source   = ('função f(x):\n'
+                    '    retorna 2*x')
+        expected = ('#coding: utf-8\n'
+                    'def f(x):\n'
+                    '    return 2*x')
         self.assertEqual(expected, transpile(source))
 
 if __name__ == '__main__':
