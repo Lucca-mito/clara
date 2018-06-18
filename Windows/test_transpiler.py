ffbf408e7a820399d07998677f5a962730111b95
Lucca-mito <luccasukman@gmail.com>
2018-06-18 12:08:41 -0300

    Consertei identação no Windows

--- Windows/test_transpiler.py
+++ Windows/test_transpiler.py
@@ -27,13 +27,13 @@ class TranspilerTDD(unittest.TestCase):
 #                    'minha_idade = 17')
 #        self.assertEqual(expected, transpile(source))
 
-   def test_array_assignment(self):
-       source   = ('numeros sao 1, 2, 3\n'
-                   'um, dois, tres sao numeros')
-       expected = ('#coding: utf-8\n'
-                   'numeros = 1, 2, 3\n'
-                   'um, dois, tres = numeros')
-       self.assertEqual(expected, transpile(source))
+    def test_array_assignment(self):
+        source   = ('numeros sao 1, 2, 3\n'
+                    'um, dois, tres sao numeros')
+        expected = ('#coding: utf-8\n'
+                    'numeros = 1, 2, 3\n'
+                    'um, dois, tres = numeros')
+        self.assertEqual(expected, transpile(source))
 
     def test_excludes_strings(self):
         source   =  '"mostra quem vc eh!"'
@@ -59,13 +59,13 @@ class TranspilerTDD(unittest.TestCase):
                     'a != b')
         self.assertEqual(expected, transpile(source))
 
-   def test_function_definition(self):
-       source   = ('funcao f(x):\n'
-                   '    retorna 2*x')
-       expected = ('#coding: utf-8\n'
-                   'def f(x):\n'
-                   '    return 2*x')
-       self.assertEqual(expected, transpile(source))
+    def test_function_definition(self):
+        source   = ('funcao f(x):\n'
+                    '    retorna 2*x')
+        expected = ('#coding: utf-8\n'
+                    'def f(x):\n'
+                    '    return 2*x')
+        self.assertEqual(expected, transpile(source))
 
     def test_property_access(self):
         source   =  'propriedade do objeto'
