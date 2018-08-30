2d09426460ecfaffd15a16ff8fca41286cab737e
Lucca-mito <luccasukman@gmail.com>
2018-08-30 20:23:52 -0300

    Did more things, my oh my

--- standard_library.py
+++ standard_library.py
@@ -1,7 +1,12 @@
 #import __builtin__
 
+# Types
 texto = str
 real = float
+
+# Functions
+booleano = bool
+mapa = map
 
 def mostra(msg):
     if isinstance(msg, bool):
@@ -10,3 +15,12 @@ def mostra(msg):
     elif isinstance(msg, float):
     	print str(msg).replace('.', ',')
     else: print msg
+
+def tamanho(*args):
+    if len(args) == 1:
+        arg = args[0]
+        if isinstance(arg, int) or isinstance(arg, float):
+            return abs(x)
+        else: return len(arg)
+    else: return len(args)
+    
